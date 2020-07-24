81ca32a68c4255eaa85f548ba5f03a222d3b63a4
emvh <emilyvho@gmail.com>
2020-07-23 18:00:01 -0700

    Load proxy server with 1 microservice

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
 const path = require('path');
-const express  = require('express');
-const app      = express();
+const express = require('express');
+const app = express();
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer({changeOrigin: true});
 const port = 3000;
+
 app.use(express.static('./public'));
 
-const justin = "http://13.57.228.197/";
-const sam = "http://13.52.76.182/";
-const matty = "https://trulia-matthew.herokuapp.com/";
-const kim = "http://13.56.248.150";
+const HomeListingImages = "http://13.52.75.41:3001/";
+// const SimilarHomes = "http://13.57.228.197/";
+// const MortgageCalculator = "http://13.56.248.150";
 
-//Justin
-app.all("/api/similarListings", function(req, res) {
-    console.log(`redirecting to justin's server`);
-    apiProxy.web(req, res, {target: justin, changeOrigin: true});
-});
-app.all("/api/nearbyListings", function(req, res) {
-    console.log(`redirecting to justin's server`);
-    apiProxy.web(req, res, {target: justin, changeOrigin: true});
+// Home Listing Images
+app.all("/api/listings/*", function(req, res) {
+    console.log(`redirecting to HomeListingImages's server`);
+    apiProxy.web(req, res, {target: HomeListingImages, changeOrigin: true});
 });
 
-// //Sam
-app.all("/api/listings", function(req, res) {
-    console.log(`redirecting to sam's server`);
-    apiProxy.web(req, res, {target: sam, changeOrigin: true});
-});
+// Similar Homes
+// app.all("/api/similarListings", function(req, res) {
+//     console.log(`redirecting to SimilarHomes's server`);
+//     apiProxy.web(req, res, {target: SimilarHomes, changeOrigin: true});
+// });
+// app.all("/api/nearbyListings", function(req, res) {
+//     console.log(`redirecting to SimilarHomes's server`);
+//     apiProxy.web(req, res, {target: SimilarHomes, changeOrigin: true});
+// });
 
-//Matt
-app.all("/reviews", function(req, res) {
-    console.log(`redirecting to matt's server`);
-    apiProxy.web(req, res, {target: matty, changeOrigin: true});
-});
-app.all("/features", function(req, res) {
-    console.log(`redirecting to matt's server`);
-    apiProxy.web(req, res, {target: matty, changeOrigin: true});
-});
-
-//Kim
-app.all("/api/home1", function(req, res) {
-    console.log(`redirecting to kim's server`);
-    apiProxy.web(req, res, {target: kim, changeOrigin: true});
-});
+// Mortgage Calculator
+// app.all("/api/home1", function(req, res) {
+//     console.log(`redirecting to MortgageCalculator's server`);
+//     apiProxy.web(req, res, {target: MortgageCalculator, changeOrigin: true});
+// });
 
 app.listen(port, () => console.log(`\nlistening at http://localhost:${port}`));
-
-//app.all = attaches to the application's router, so it's used whenever the app.router middleware is reached (which handles all the method routes... GET, POST, etc)
